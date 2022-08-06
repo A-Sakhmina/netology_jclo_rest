7fc7d0b15a0e07d8a62f577056dbce88bac64215
Sakhmina <asadullaevasakhmina@gmail.com>
2022-08-06 15:38:20 +0300

    Add exception handlers

--- src/main/java/com/sakhmina/controller/AuthorizationController.java
+++ src/main/java/com/sakhmina/controller/AuthorizationController.java
@@ -5,6 +5,9 @@ import com.sakhmina.domain.User;
 import com.sakhmina.exceptions.InvalidCredentials;
 import com.sakhmina.exceptions.UnauthorizedUser;
 import com.sakhmina.service.AuthorizationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +25,17 @@ public class AuthorizationController {
     @GetMapping("/authorize")
     public List<Authorities> getAuthorities(@Valid User user) throws InvalidCredentials, UnauthorizedUser {
         return service.getAuthorities(user);
+    }
+
+    @ExceptionHandler(InvalidCredentials.class)
+    public ResponseEntity<String> icHandler(InvalidCredentials e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnauthorizedUser.class)
+    public ResponseEntity<String> uuHandler(UnauthorizedUser e) {
+        System.out.println(e.getMessage());
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
 }
